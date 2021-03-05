f33989ed799470150854f711354ff94490d60eb7
Jonatan Schneevoigt <jonatan.gud.hum@gmail.com>
2021-03-05 14:01:02 +0100

    Remove the functions that are now in the header file

--- source/main.c
+++ source/main.c
@@ -2,71 +2,9 @@
 #include <stdio.h>
 #include <man.h>
 #include <woman.h>
+#include "sprites.h"
 
-// The pistol (man) sprite
-typedef struct 
-{
-	int x;
-	int y;
-
-	u16* sprite_gfx_mem;
-	u8*  frame_gfx;
-
-	int state;
-	int anim_frame;
-}Man;
-
-// The bullet (woman) sprite
-typedef struct
-{
-	int x;
-	int y;
-
-	u16* sprite_gfx_mem[12];
-	int gfx_frame;
-
-	int state;
-	int anim_frame;
-
-}Woman;
-
-enum SpriteState {W_UP = 0, W_RIGHT = 1, W_DOWN = 2, W_LEFT = 3};
-
-void animateMan(Man *sprite)
-{
-	int frame = sprite->anim_frame + sprite->state;
-
-	u8* offset = sprite->frame_gfx + frame * 32*32;
-
-	dmaCopy(offset, sprite->sprite_gfx_mem, 32*32);
-}
-
-void initMan(Man *sprite, u8* gfx)
-{
-	sprite->sprite_gfx_mem = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
-	
-	sprite->frame_gfx = (u8*)gfx;
-}
-
-void animateWoman(Woman *sprite)
-{
-	sprite->gfx_frame = sprite->anim_frame + sprite->state;
-}
-
-void initWoman(Woman *sprite, u8* gfx)
-{
-	int i;
-
-	for(i = 0; i < 12; i++)
-	{
-		sprite->sprite_gfx_mem[i] = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
-		dmaCopy(gfx, sprite->sprite_gfx_mem[i], 32*32);
-		gfx += 32*32;
-	}
-}
-//---------------------------------------------------------------------
 int main(void) 
-//---------------------------------------------------------------------
 {
 	Man man = {0,0};
 	Woman woman = {0,0};
